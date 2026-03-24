e132956ff27eb5e6cb5fc54cddaf5e66b210ed06
Mateus-Sebastiao <mateussebastiao2003@gmail.com>
2026-03-24 20:34:22 +0200

    Update tests file with import ActiveProfiles

--- springboot_app/src/test/java/com/mateus/practical/homeops/GitopsApplicationTests.java
+++ springboot_app/src/test/java/com/mateus/practical/homeops/GitopsApplicationTests.java
@@ -2,6 +2,7 @@ package com.mateus.practical.homeops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // Added it
 
 @SpringBootTest
 @ActiveProfiles("test") // Added it
