96e8adaa139a5edce56d310f9224e68b993f175c
Mateus-Sebastiao <mateussebastiao2003@gmail.com>
2026-03-26 12:41:45 +0200

    Moving to test IntegrationTests

--- springboot_app/src/test/java/com/mateus/practical/homeops/UserRepositoryIntegrationTest.java
+++ springboot_app/src/test/java/com/mateus/practical/homeops/UserRepositoryIntegrationTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class UserRepositoryIntegrationTest {
 
     @Container
